8d55e1e7934bd08e5845e8986d2076b224cc7cc6
khalford <144675319+khalford@users.noreply.github.com>
2023-11-30 15:41:52 +0000

    Create stats.php

---
+++ stats.php
@@ -0,0 +1,12 @@
+<?php
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database_name", $username, $password);
+  echo "<h2>Most Commits SCD-Openstack-Utils</h2><ol>";
+  foreach($db->query("SELECT username,no_commits FROM $table_name") as $row) {
+          echo "<li>".$row['username']."'s ".'commits: '.$row['no_commits']."</li>";
+  }
+  echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
