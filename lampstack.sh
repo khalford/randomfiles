e8d467d10f9f78df3cee5f09452f9112ba94af27
khalford <144675319+khalford@users.noreply.github.com>
2023-11-30 16:12:49 +0000

    Update lampstack.sh

--- lampstack.sh
+++ lampstack.sh
@@ -4,7 +4,6 @@
 # If script run as sudo get sudo caller username
 username=$USER
 if [ "$username" = root ]; then username=$SUDO_USER; fi
-export $username
 
 # Install Apache2
 apt update
@@ -49,7 +48,31 @@ sed -i -e "s/<username>/$username/g" ./stats.php
 sed -i -e "s/<password>/"password"/g" ./stats.php
 cp ./stats.php /var/www/${domain_name}/stats.php
 
-# Call mysql.sh to do mysql things
-bash ./mysql.sh
+# Assign variables
+database_name="GitHubStats"
+passwd="password"
+
+# Create the MySQL database
+mysql -e "CREATE DATABASE ${database_name};"
+
+# Create the user in MySQL
+mysql -e "CREATE USER '${username}'@'%' IDENTIFIED BY '$passwd';"
+
+# Grant the user permissions to the database
+mysql -e "GRANT ALL ON ${database_name}.* TO '${username}'@'%';"
+
+# Assign table name
+table_name="commit_stats"
+
+# Assign the password
+password="password"
+
+# Create the table in the database
+mysql -u $username -p$password -e "CREATE TABLE ${database_name}.${table_name} (
+user_id INT AUTO_INCREMENT,
+username VARCHAR(255),
+no_commits INT,
+PRIMARY KEY(user_id)
+);"
 
 
