c8168d470394aaa26e4967fedaa4cc598ad213cd
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2023-12-01 10:47:32 +0000

    Fixed github_scrap.sh

--- github_scrape.sh
+++ github_scrape.sh
@@ -16,6 +16,7 @@ sed -i 's/,/, /g' ./contributor_commits
 
 # Add to MySql table each line
 while read line; do
-	commiter=$( cut -d ", " -f 1 )
-	no_commits=$( cut -d ", " -f 2 )
-	mysql -u$username -ppassword -e "INSERT INTO GitHubStats.commit_stats (username, no_commits) VALUES (${comitter}, ${no_commits});"done < ./contributor_commits
+        commiter=$( awk -F', ' '{ print $1 }' <<< $line)
+        no_commits=$( awk -F', ' '{ print $2 }' <<< $line)
+        mysql -u"$username" -ppassword -e "INSERT INTO GitHubStats.commit_stats (username, no_commits) VALUES ('${commiter}', '${no_commits}');"
+done < ./contributor_commits
