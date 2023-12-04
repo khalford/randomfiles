21235ab3500adc05d14ee7558925d7234c25b8ea
khalford <144675319+khalford@users.noreply.github.com>
2023-12-04 13:24:52 +0000

    Update lampstack.sh

--- lampstack.sh
+++ lampstack.sh
@@ -86,6 +86,6 @@ mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor |  tee /etc/apt/keyrings/grafana.gpg > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" |  tee -a /etc/apt/sources.list.d/grafana.list
 apt-get update
-apt-get install grafana
+apt-get install grafana -y
 
 
